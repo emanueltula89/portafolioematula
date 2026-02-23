b9d5f10378d1e11602405db38674ab74245b9768
Ema <faunapescaycaza@gmail.com>
2026-02-23 07:28:09 -0300

    Preparar para despliegue en Vercel (remover configuracion estatica)

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
     images: {
         unoptimized: true,
     },
-    basePath: '/portafolioematula',
-    assetPrefix: '/portafolioematula',
 };
 
 export default nextConfig;
